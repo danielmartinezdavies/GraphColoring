6a89c49299f22c5f84894541cb007542990d9ab8
danielmartinezdavies <danielmartinezdavies@gmail.com>
2022-01-05 13:03:43 +0100

    Add find_package boost to CMakeLists.txt

--- main.cpp
+++ main.cpp
@@ -7,9 +7,6 @@ int main() {
     Graph g("../examples/input_file.txt");
 
     std::cout << "Number of unique colors: " << g.getNumColorsUsed();
-
     g.exportToDot("output.txt");
-
-
     return 0;
 }
